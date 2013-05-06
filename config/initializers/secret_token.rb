24114e594f4215f470a40fd14ef91c364f964cff
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-05-06 13:11:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Missionleaderapp::Application.config.secret_token = '727df8f8264c0c27a7b5faf560c70f1d1c843fb50568669ff74ecd2d2069f8283f9b096f13bc5bf925c80b827e59b4bd9e3419465696acbf1f5de94917388db2'
